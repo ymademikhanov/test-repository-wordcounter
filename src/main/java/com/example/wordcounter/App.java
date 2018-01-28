d6cb9c096e40f973215bf23c4fcac10d2575ace6
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-01-28 14:00:28 +0600

    Corrected spacing in the code

--- src/main/java/com/example/wordcounter/App.java
+++ src/main/java/com/example/wordcounter/App.java
@@ -9,10 +9,10 @@ public class App
     
     public static int wordcount(String s) {
        	int counter = 0, length = s.length();
-		for (int i = 0; i < length; i++) {
-			if (s.charAt(i) != ' ' && (i == length - 1 || s.charAt(i + 1) == ' '))
-				counter++;
-		}
-		return counter;
+	for (int i = 0; i < length; i++) {
+		if (s.charAt(i) != ' ' && (i == length - 1 || s.charAt(i + 1) == ' '))
+			counter++;
+	}
+	return counter;
     }
 }
